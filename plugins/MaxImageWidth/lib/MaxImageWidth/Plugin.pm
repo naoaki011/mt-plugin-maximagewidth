029dfa48a24ab6828684bd7e634ba2d0d22747ea
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-12-01 21:43:51 +0900

    add switch to MT4.x.

--- plugins/MaxImageWidth/lib/MaxImageWidth/Plugin.pm
+++ plugins/MaxImageWidth/lib/MaxImageWidth/Plugin.pm
@@ -9,12 +9,21 @@ sub xfrm_asset_options {
     my $config        = $plugin->get_config_hash( 'blog:' . $blog->id );
     if ($config->{max_image_width}) {
         my $slug;
-        $slug = <<END_TMPL;
+        if (MT->version_id >= 5) {
+            $slug = <<END_TMPL;
 <link rel="stylesheet" type="text/css" href="<mt:var name="static_uri">plugins/MaxImageWidth/app.css" />
 <link rel="stylesheet" type="text/css" href="<mt:var name="static_uri">plugins/MaxImageWidth/css/ui-lightness/jquery-ui.css" />
-<script type="text/javascript" src="<mt:var name="static_uri">jqueryui/ui.core.js"></script>
-<script type="text/javascript" src="<mt:var name="static_uri">jqueryui/ui.slider.js"></script>
 END_TMPL
+        }
+        else {
+            $slug = <<END_TMPL;
+<link rel="stylesheet" type="text/css" href="<mt:var name="static_uri">plugins/MaxImageWidth/app.css" />
+<link rel="stylesheet" type="text/css" href="<mt:var name="static_uri">jquery/themes/flora/flora.all.css" />
+<script type="text/javascript" src="<mt:var name="static_uri">jquery/jquery.js"></script>
+<script type="text/javascript" src="<mt:var name="static_uri">jquery/ui/ui.core.js"></script>
+<script type="text/javascript" src="<mt:var name="static_uri">jquery/ui/ui.slider.js"></script>
+END_TMPL
+        }
         $$tmpl =~ s{(<mt:setvarblock name="html_head" append="1">)}{$1 $slug}msi;
     }
 }
@@ -47,7 +56,7 @@ sub asset_options_param {
                 help_section => "creating_thumbnails"
                 }
         ) or return $app->error('cannot create the thumbnail element');
-        my $mt = ($param->{make_thumb} ? 'checked="checked"' : '');
+        my $mt = ($param->{make_thumb} ? ' checked="checked"' : '');
         my $html = <<HTML;
 <script type="text/javascript">
     var full_width = $param->{width};
@@ -55,7 +64,7 @@ sub asset_options_param {
     var max_width = $max_width;
     jQuery(document).ready( function() {
         if (full_width > max_width) {
-             jQuery('#create_thumbnail').attr('checked', true).click(function(event){ jQuery(this).attr('checked', true); });
+             jQuery('#create_thumbnail').replaceWith('<input type="hidden" name="thumb" id="create_thumbnail" value="1"$mt />');
         }
         jQuery('#thumb_width').change( function() {
             var new_w = jQuery(this).val();
@@ -76,11 +85,11 @@ sub asset_options_param {
             jQuery('#width-slider').slider('option', 'value', new_w);
         });
         jQuery('#width-slider').slider({ 
-          slide: function(event, ui) {
-              jQuery('#thumb_width').val( ui.value );
-              jQuery('#thumb_height').val( Math.floor( (full_height * ui.value ) / full_width) );
-          },
-          max: max_width
+            slide: function(event, ui) {
+                jQuery('#thumb_width').val( ui.value );
+                jQuery('#thumb_height').val( Math.floor( (full_height * ui.value ) / full_width) );
+            },
+            max: max_width
         });
         jQuery('#width-slider').slider('value', max_width);
         if (full_width > max_width) {
@@ -89,8 +98,8 @@ sub asset_options_param {
     });
 </script>
         <div id="create_thumbnail_cb">
-          <input type="checkbox" name="thumb" id="create_thumbnail" value="1" $mt />
-          <label for="create_thumbnail"><__trans_section component="MaxImageWidth"><__trans phrase='Use thumbnail?'></__trans_section></label>
+          <input type="checkbox" name="thumb" id="create_thumbnail" value="1"$mt />
+          <label><__trans_section component="MaxImageWidth"><__trans phrase='Use thumbnail?'></__trans_section></label>
         </div>
         <div id="w-h">
           <input type="text" id="thumb_width" size="3" name="thumb_width" value="$param->{width}" /> x <input type="text" id="thumb_height" size="3" name="thumb_height" value="$param->{height}" />
@@ -99,7 +108,7 @@ sub asset_options_param {
 HTML
         $new_field->innerHTML($html);
         $tmpl->insertAfter( $new_field, $ct_field )
-      or return $app->error('failed to insertAfter.');
+          or return $app->error('failed to insertAfter.');
         $ct_field->innerHTML('');
 
         $param;
